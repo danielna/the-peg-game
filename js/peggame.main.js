48141793866c41e63a81fd276c0bda8f2c702f5c
Daniel Na <daniel.na@gmail.com>
2012-10-08 22:46:32 -0400

    add another test, but with a different starting point

--- js/peggame.main.js
+++ js/peggame.main.js
@@ -4,29 +4,59 @@ $(document).ready(function() {
 
     // the following moves result in a game that ends in 3 pieces.
     // commented letters are the pegs removed at each step.
-    
-    pegBoard.start_game("A");
 
-    pegBoard.move_peg("D", "A"); //B
+    // pegBoard.start_game("A");
 
-    pegBoard.move_peg("K", "D"); //G
+    // pegBoard.move_peg("D", "A"); //B
 
-    pegBoard.move_peg("M", "K"); //L
+    // pegBoard.move_peg("K", "D"); //G
 
-    pegBoard.move_peg("O", "M"); //N
+    // pegBoard.move_peg("M", "K"); //L
 
-    pegBoard.move_peg("F", "O"); //J
+    // pegBoard.move_peg("O", "M"); //N
 
-    pegBoard.move_peg("A", "F"); //C
+    // pegBoard.move_peg("F", "O"); //J
 
-    pegBoard.move_peg("E", "L"); //H
+    // pegBoard.move_peg("A", "F"); //C
 
-    pegBoard.move_peg("L", "N"); //M
+    // pegBoard.move_peg("E", "L"); //H
 
-    pegBoard.move_peg("F", "M"); //I
+    // pegBoard.move_peg("L", "N"); //M
 
-    pegBoard.move_peg("N", "L"); //M
+    // pegBoard.move_peg("F", "M"); //I
 
-    pegBoard.move_peg("K", "M"); //L
+    // pegBoard.move_peg("N", "L"); //M
+
+    // pegBoard.move_peg("K", "M"); //L
+
+
+
+
+    // and two end pieces!  also a different start point.
+    // pegBoard.start_game("B");
+
+    // pegBoard.move_peg("G", "B"); //D
+
+    // pegBoard.move_peg("F", "D"); //E
+
+    // pegBoard.move_peg("O", "F"); //J
+
+    // pegBoard.move_peg("B", "G"); //D
+
+    // pegBoard.move_peg("L", "E"); //H
+
+    // pegBoard.move_peg("F", "D"); //E
+
+    // pegBoard.move_peg("N", "L"); //M
+
+    // pegBoard.move_peg("K", "M"); //L
+
+    // pegBoard.move_peg("M", "F"); //I
+
+    // pegBoard.move_peg("G", "B"); //D
+
+    // pegBoard.move_peg("A", "D"); //B
+
+    // pegBoard.move_peg("C", "J"); //F
 
 });
